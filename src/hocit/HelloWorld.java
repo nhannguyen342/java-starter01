f3ad631788a99d46356f88941b0815f540448528
Code With Wingsgles <Danganhnguyet666@gmail.com>
2025-04-27 23:36:09 +0700

    scanner

--- src/hocit/HelloWorld.java
+++ src/hocit/HelloWorld.java
@@ -1,10 +1,15 @@
 package hocit;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World with Java");
-		System.out.println("Hello World with Java");
-		System.out.println("Hello World with Java");
-		System.out.println("Hello World with Java");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
 	}
 }
