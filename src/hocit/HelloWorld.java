e807d7ee547f6fe2bd934e86bd99b48a5a157b40
Code With Wingsgles <Danganhnguyet666@gmail.com>
2025-04-27 16:05:11 +0700

    init project

---
+++ src/hocit/HelloWorld.java
@@ -0,0 +1,10 @@
+package hocit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with Java");
+		System.out.println("Hello World with Java");
+		System.out.println("Hello World with Java");
+		System.out.println("Hello World with Java");
+	}
+}
